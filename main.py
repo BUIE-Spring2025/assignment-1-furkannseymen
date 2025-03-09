4c8ac102a12774b65c5cbaadebab2a674276d79d
furkannseymen <furkan.seymen@std.bogazici.edu.tr>
2025-03-09 12:40:35 +0300

    Assignment 1

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
+    Roman_numeral = ''
+    while num > 0:
+        if num >= 1000:
+            Roman_numeral += 'M'
+            num -= 1000
+        elif num >= 900:
+            Roman_numeral += 'CM'
+            num -= 900
+        elif num >= 500:
+            Roman_numeral += 'D'
+            num -= 500
+        elif num >= 400:
+            Roman_numeral += 'CD'
+            num -= 400
+        elif num >= 100:
+            Roman_numeral += 'C'
+            num -= 100
+        elif num >= 90: 
+            Roman_numeral += 'XC'
+            num -= 90
+        elif num >= 50:
+            Roman_numeral += 'L'
+            num -= 50
+        elif num >= 40:
+            Roman_numeral += 'XL'
+            num -= 40
+        elif num >= 10:
+            Roman_numeral += 'X'
+            num -= 10
+        elif num >= 9:
+            Roman_numeral += 'IX'
+            num -= 9
+        elif num >= 5:
+            Roman_numeral += 'V'
+            num -= 5
+        elif num >= 4:
+            Roman_numeral += 'IV'
+            num -= 4
+        elif num >= 1:
+            Roman_numeral += 'I'
+            num -= 1
+    return Roman_numeral
 
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
